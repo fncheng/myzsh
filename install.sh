8c8ef21243cd6eb873d5be3c9d4b3fdcb1f49a7a
siwadiya <37958055+fncheng@users.noreply.github.com>
2021-02-21 17:52:08 +0800

    Update install.sh
    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mv ../myzsh/* ~/.oh-my-zsh/custom
+rm -rf ~/.oh-my-zsh/custom/* -y && git clone https://github.com/fncheng/myzsh.git ~/.oh-my-zsh/custom
+mv ~/.oh-myzsh/custom/.zshrc ~
